9ab72b95a1fb1701a194f025a2502176c9bcb7c6
Landysh Khusaenova <landysh.kg@gmail.com>
2023-12-10 14:03:06 +0100

    update_cypress_spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,8 @@
-describe('example to-do app', () => {
+describe("my first cypress_test", () => {
   beforeEach(() => {
-    cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/')
-  })
-  it('element exist', () => {
-  cy.get('.table-responsive').should('exist');
-})
-})
+    cy.visit("https://sqlverifier-live-6e21ca0ed768.herokuapp.com/");
+  });
+  it("element exist", () => {
+    cy.get(".table-responsive").should("exist");
+  });
+});
