93bed59f798505573c24e9999d3ad1f6ab1f629d
Landysh Khusaenova <landysh.kg@gmail.com>
2023-12-10 21:47:15 +0100

    update files

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "qfke5b",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
