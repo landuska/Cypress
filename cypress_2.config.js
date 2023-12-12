739dc555c9b4d79d7d01c8ebbf191bda60fe6972
Landysh Khusaenova <landysh.kg@gmail.com>
2023-12-12 21:58:11 +0100

    Homework 5.2.3_new_student

--- cypress_2.config.js
+++ cypress_2.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "qfke5b",
-  username: "admin",
-  password: "admin",
+  username: "lan_student",
+  password: "student",
   e2e: {
     baseUrl: "https://sqlverifier-staging-08050d656f7a.herokuapp.com",
     setupNodeEvents(on, config) {
